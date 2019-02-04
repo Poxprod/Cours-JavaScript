b96e2afa69803aa46a9a7cc99ecc96fb5722a891
Philippe BOLONKIN <47108562+Poxprod@users.noreply.github.com>
2019-02-04 23:10:13 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,8 @@
+/* function calculCarre(nb) {
+  let resultat = nb ** 2;
+  return(resultat);
+} */
+function hypoth(cote,oppose) {
+
+return Math.sqrt(cote ** 2 + oppose ** 2)
+}
---
+++ script-exo1.js
@@ -0,0 +1,30 @@
+
+  var cote1 = prompt("Veuillez saisir la longueur du côté adjacent:");
+  var oppose1 = prompt("Veuillez saisir la longueur du côté opposé :");
+  alert("L'hypothénuse d'un triangle de " + cote1 + " sur " + oppose1 + " est égale à " + hypoth(cote1,oppose1))
+
+
+
+
+/* Affichage du résultat dans la console
+    console.log(reponse);
+    document.write("<h1>"+reponse+"</h1>");*/
+
+/* var resultat = "<ul>";
+var compteur = 0;
+
+for (var i = -12728; i <= 16856; i += 17) {
+/*  resultat = resultat + ("<li>" + i + "</li>"); Ce qui suit est une autre façon d'écrire -> */
+/*  = attribuer une valeur; == opérateur de compraison; ===
+
+    if (i % 23 == 0 ) {
+      compteur ++;
+      resultat += "<li>" + i + "</li>";
+    }
+}
+resultat = resultat + "</ul>";
+document.write(resultat);
+alert("Nombre d'items : "+compteur);
+var truc = calculCarre(10);
+console.log(truc);
+*/
